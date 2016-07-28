8d11c229d475b3c175eeb2e1129475b465e18b92
Barrantes <gerson.barrantes@covidien.com>
2016-07-28 14:28:12 -0600

    Tarea 2

--- Statements/Program/Program.cs
+++ Statements/Program/Program.cs
@@ -13,14 +13,14 @@ namespace Program
             bool proceed = true;
             while (proceed)
             {
-                char selecction = printMenu();
-                switch (selecction)
+               char selection = printMenu();
+                switch (selection)
                 {
                     case 'a':
                         addValues();
                         break;
                     case 'b':
-                        concatenateStrings();
+                        concatenationStrings();
                         break;
                     case 'c':
                         loopANumber();
@@ -31,36 +31,82 @@ namespace Program
                         Console.ReadKey();
                         break;
                     default:
-                        Console.WriteLine("Opción incorrecta.");
+                        Console.WriteLine("Opcion Incorrecta");
                         break;
                 }
-            }
+
+                Console.ReadKey();
+
+            } 
         }
 
         private static void loopANumber()
         {
-            throw new NotImplementedException();
-        }
+            int value;
 
-        private static void concatenateStrings()
+            Console.WriteLine("Por Favor ingrese un numero");
+            Console.ReadLine();
+            value = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 0; i < value; i++)
+
+            {
+
+                Console.WriteLine("El resultado es:" + i);
+            }
+
+
+        }
+               
+
+        private static void concatenationStrings()
         {
-            throw new NotImplementedException();
+
+            Console.WriteLine("Ingrese el primer texto: ");
+            Console.ReadLine();
+            string text1 = Convert.ToString(Console.ReadLine());
+            
+
+            Console.WriteLine("Ingrese el segundo texto: ");
+            string text2 = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("El resultado es: " + text1 + text2);
+
+
+
         }
 
         private static void addValues()
-        {
-            throw new NotImplementedException();
+        { 
+            
+            int v1, v2,r ;
+            Console.WriteLine("Ingrese un numero:");
+            Console.ReadLine();
+            v1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese otro numero:");
+            v2 = Convert.ToInt32(Console.ReadLine());
+         
+            r = v1 + v2;
+            Console.WriteLine("El resultado es:" + r);
+            Console.ReadLine();
+            Console.ReadKey();
+
         }
+
+
 
         static char printMenu()
         {
-            Console.WriteLine("Seleccione una opción:");
-            Console.WriteLine("a-Sumar dos valores.");
-            Console.WriteLine("b-Concatenar dos strings.");
-            Console.WriteLine("c-Iterar un número.");
-            Console.WriteLine("d-Salir.");
+            Console.WriteLine("Seleccione una opcion:");
+            Console.WriteLine("a - Sumar dos valores.");
+            Console.WriteLine("b - Concantenar dos Strings.");
+            Console.WriteLine("c - Iterar un numero.");
+            Console.WriteLine("d - Salir.");
             char option = (char)Console.Read();
             Console.WriteLine();
             return option;
         }
     }
+}
